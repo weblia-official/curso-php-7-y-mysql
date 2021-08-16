7823ff16b9b7872c39002e312fe59db76d6fafa0
Weblia <88990648+weblia-official@users.noreply.github.com>
2021-08-15 21:04:59 -0400

    Add files via upload

---
+++ 2.6 integrar.php
@@ -0,0 +1,29 @@
+
+<!-- Integrando PHP en HTML -->
+
+<h1> <?php echo "Hola, estamos integrando PHP en HTML"; ?> </h1>
+
+<?php
+
+//Integrando HTML en PHP
+
+echo "<h3>Hola, estoy integrando HTML en PHP</h3>";
+
+//Ahora un poco más complicado
+
+echo "<h2 class='rojo'>Estoy integrando HTML en PHP con distintas comillas</h2>";
+
+$variable = TRUE;
+
+if($variable == 1){
+    echo "Hola";
+}
+?>
+
+<style>
+.rojo{
+    color:red;
+}
+
+</style>
+
